efd6362745e8a41fa4067b25a2d0d8bbd7ee7ce2
Garefen <gabrielbsarti@gmail.com>
2019-08-11 13:27:23 -0300

    init

---
+++ backend/src/controllers/LikeController.js
@@ -0,0 +1,38 @@
+const Dev = require('../models/Dev');
+
+module.exports = {
+    async store(request, response) {
+
+        console.log(request.io, request.connectedUsers);
+
+        const { devId } = request.params;
+        const { user } = request.headers;
+
+        const loggedDev = await Dev.findById(user);
+        const targetDev = await Dev.findById(devId);
+
+        if (!targetDev) {
+            return response.status(400).json({ error: 'Dev not exists' });
+        }
+
+        if (targetDev.likes.includes(user)) {
+            const loggedSocket = request.connectedUsers[user];
+            const targetSocket = request.connectedUsers[devId];
+
+            if (loggedSocket) {
+                request.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetDev) {
+                request.io.to(targetSocket).emit('match', loggedDev);
+            }
+        }
+
+        loggedDev.likes.push(targetDev._id);
+
+        await loggedDev.save();
+
+
+        return response.json( loggedDev );
+    }
+};
